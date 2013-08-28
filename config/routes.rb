ad3ffa491b61fd508e1ed43c9b35b94c35a843da
cway <cway@knock-pro.local>
2013-08-28 11:02:44 +0800

    x

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 EcAdmin::Application.routes.draw do
   devise_for :users
   root :to => 'main#index'
+  get  '/assets/img/:name'          => redirect("/assets/%{name}")
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
