a7ad430886e5691a9cced6eb3b34b5cf2d8ada53
cway <cway@knock-pro.local>
2013-08-28 14:27:43 +0800

    x

--- app/models/eav_attribute.rb
+++ app/models/eav_attribute.rb
@@ -40,4 +40,10 @@ class EavAttribute < ActiveRecord::Base
      self
   end 
 
+  def verify_params( params, key )
+    unless params.has_key? key
+  		raise ArgumentError, "no params named #{key}" 
+  	end
+  end
+  
 end
