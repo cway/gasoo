796ad264139d9d7a73333529cd5f12127374d28b
cway <cway@knock-pro.local>
2013-08-28 12:04:19 +0800

    x

--- app/models/entity_type.rb
+++ app/models/entity_type.rb
@@ -2,7 +2,8 @@
 #author cway 2013-07-09
 
 class EntityType < BaseModel
-  attr_accessible :entity_type_id, :entity_type_code, :default_attribute_set_id
+  attr_accessor :entity_type_id, :entity_type_code, :default_attribute_set_id
+  
   self.table_name  = "eav_entity_type"
   validates :entity_type_id, :entity_type_code, :presence => true
   
