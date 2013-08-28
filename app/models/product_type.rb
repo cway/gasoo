19d30d4ee88c2c327c131c08f0cc1ce748fce543
cway <cway@knock-pro.local>
2013-08-28 16:56:54 +0800

    x

---
+++ app/models/product_type.rb
@@ -0,0 +1,17 @@
+#encoding: utf-8
+#author cway 2013-07-15
+
+class ProductType < ActiveRecord::Base
+  validates       :product_type_name, :presence => true
+  set_table_name  "product_type"
+
+  def self.all_with_primary_key
+    types                                  =  self.all
+    type_list                              =  Hash.new
+    types.each do |type_info|
+      type_list[type_info.product_type_id] = type_info.product_type_name
+    end
+
+    type_list
+  end
+end
