954cc54cdb547e6a9d8610aa3781121c16bc3bf5
cway <cway@knock-pro.local>
2013-08-29 18:19:16 +0800

    x

--- app/models/event_product.rb
+++ app/models/event_product.rb
@@ -8,7 +8,7 @@ class EventProduct < ActiveRecord::Base
    validates :rule_id, :product_id, :presence => true
 
    def self.get_daily_flashsales
-     find_by_sql("select eventrule.rule_id, eventrule.from_date, count(event_product.event_product_id) as cnt from eventrule left join event_product on eventrule.rule_id = event_product.rule_id where eventrule.parent_rule_id = 1 group by event_product.rule_id") 
+     Eventrule.find_by_sql("select eventrule.rule_id, eventrule.from_date, count(event_product.event_product_id) as cnt from eventrule left join event_product on eventrule.rule_id = event_product.rule_id where eventrule.parent_rule_id = 1 group by event_product.rule_id") 
    end
    
    def self.get_products_name( product_ids )
@@ -16,7 +16,7 @@ class EventProduct < ActiveRecord::Base
       return
     end 
    
-    name_list                       = find_by_sql("select product_entity_varchar.value, product_entity_varchar.entity_id from eav_attribute left join product_entity_varchar on product_entity_varchar.attribute_id = eav_attribute.attribute_id  and product_entity_varchar.entity_id in (#{product_ids.join(',')})  where eav_attribute.`attribute_code` = 'name' and eav_attribute.`entity_type_id` = #{ApplicationController::PRODUCT_TYPE_ID}")
+    name_list                       = EavAttribute.find_by_sql("select product_entity_varchar.value, product_entity_varchar.entity_id from eav_attribute left join product_entity_varchar on product_entity_varchar.attribute_id = eav_attribute.attribute_id  and product_entity_varchar.entity_id in (#{product_ids.join(',')})  where eav_attribute.`attribute_code` = 'name' and eav_attribute.`entity_type_id` = #{ApplicationController::PRODUCT_TYPE_ID}")
     ret_data                        = Hash.new
     name_list.each do | name_info |
       ret_data[name_info.entity_id] = name_info.value
