15b279e0be10b3f4804aa55182549f5ad8fb40b0
cway <cway@knock-pro.local>
2013-08-30 00:33:50 +0800

    x

--- app/models/sales_order.rb
+++ app/models/sales_order.rb
@@ -2,6 +2,6 @@
 #author cway 2013-07-11
 
 class SalesOrder < ActiveRecord::Base
-  attr_accessible :entity_id, :status, :state, :customer_id, :grand_total, :created_at
+  #attr_accessible :entity_id, :status, :state, :customer_id, :grand_total, :created_at
   self.table_name = "sales_order"
 end
