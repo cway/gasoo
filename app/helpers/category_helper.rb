8cbf0baf7d1321db65e12911922dcb751baee31a
cway <cway@knock-pro.local>
2013-08-29 16:20:31 +0800

    x

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -1,18 +1,15 @@
 #encoding: utf-8
 #author cway 2013-07-18
 
-module CategoryHelper
-  @@category_list = nil
+module CategoryHelper 
 
-  def get_name_path( ids_path )
-    unless @@category_list
-      @@category_list = Category.all_by_id_index
-    end
+  def get_name_path( ids_path ) 
+    category_list     = Category.all_by_id_index
     name_path         = ""
     ids_path.split('/').each do |id|
-      name_path      << @@category_list[id.to_i] << "/"
+      name_path      << category_list[id.to_i] << "/"
     end
     
-    return name_path
+    name_path
   end
 end
