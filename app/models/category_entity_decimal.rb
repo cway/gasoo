eac60c69bb9bd8793dc03a89ddec7bfa706030a0
cway <cway@knock-pro.local>
2013-08-29 14:27:16 +0800

    x

---
+++ app/models/category_entity_decimal.rb
@@ -0,0 +1,7 @@
+#encoding: utf-8
+#author cway 2013-08-29
+
+class CategoryEntityDecimal < ActiveRecord::Base
+  #attr_accessible :value_id, :entity_type_id, :attribute_id, :entity_id, :value
+  self.table_name = "category_entity_decimal"
+end
---
+++ app/models/category_entity_media_gallery.rb
@@ -0,0 +1,7 @@
+#encoding: utf-8
+#author cway 2013-08-29
+
+class CategoryEntityMediaGallery < ActiveRecord::Base
+  #attr_accessible :value_id, :entity_type_id, :attribute_id, :entity_id, :value
+  self.table_name = "category_entity_media_gallery"
+end
