30fef1106e252039eb4d347ffbea1112da94953a
cway <cway@knock-pro.local>
2013-08-29 15:01:08 +0800

    x

--- app/models/category.rb
+++ app/models/category.rb
@@ -107,6 +107,78 @@ class Category < ActiveRecord::Base
       return attribute_id
   end
 
+  def self.update_category( category )
+    name_attribute_id                    = Category.get_attribute_id("name")
+    category_params                      = params[:category]
+    category_params["entity_type_id"]    = ApplicationController::CATEGORY_TYPE_ID
+    parent_id                            = category_params["parent_id"].to_i
+    
+    if parent_id != 0
+      begin
+        parent_category                  = Category.find( parent_id )
+      rescue
+        parent_category                  = nil
+      end
+      category_params['parnet_id']       = parent_id
+      category_params['level']           = parent_category.level + 1
+    else
+      category_params['parent_id']       = 0
+      category_params['level']           = 1 
+    end 
+   
+    begin
+     Category.transaction do 
+       if parent_category
+         category_params["path"]         = parent_category.path + "/" + category.entity_id.to_s
+       else
+         category_params["path"]         = category.entity_id.to_s
+       end 
+
+       if category_params['parnet_id']  != category.parent_id
+         if parent_category
+           parent_category.children_count += 1
+           parent_category.save
+         end
+
+         begin
+           old_parent_category           = Category.find( category.parent_id )
+         rescue
+           old_parent_category           = nil
+         end 
+
+         if old_parent_category
+           old_parent_category.children_count -= 1
+           old_parent_category.save
+         end
+       end
+
+       if category.level               != category_params['level']
+         Category.update_level( category.entity_id, category_params['level'], category_params['path'] )
+       end
+      
+       category.update_attributes( { :parent_id => category_params['parent_id'], :path => category_params['path'], :level => category_params['level'], :updated_at => DateTime.now } )
+      
+       entity_name                       = Hash.new
+       entity_name["attribute_id"]       = name_attribute_id
+       entity_name["entity_id"]          = category.entity_id
+       entity_name["entity_type_id"]     = ApplicationController::CATEGORY_TYPE_ID
+       entity_name["value"]              = params[:category][:name]
+
+       entity_option                     = {:entity_type_id => ApplicationController::CATEGORY_TYPE_ID, :attribute_id => name_attribute_id, :entity_id => category.entity_id}
+       entity_value                      = CategoryEntityVarchar.first( :conditions => entity_option )
+       if entity_value
+         entity_value.update_attribute( 'value', entity_name["value"] )
+       else
+         entity_value                    = CategoryEntityVarchar.new(entity_name);     
+         entity_value.save
+       end 
+     end 
+  
+    rescue => err
+      raise err.message
+    end 
+  end
+
   def self.update_level( category_id, category_level, category_path )
     category_children           = self.where( { parent_id: category_id.to_i } )
     unless category_children.empty?
@@ -137,4 +209,5 @@ class Category < ActiveRecord::Base
     end
     children
   end
+
 end
