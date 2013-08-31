75e8237d1c932ab5f1e1556567917840fe5d21bc
cway <cway@knock-pro.local>
2013-08-31 14:41:56 +0800

    x

--- app/controllers/sales_orders_controller.rb
+++ app/controllers/sales_orders_controller.rb
@@ -4,24 +4,14 @@
 class SalesOrdersController < ApplicationController
   
   # GET /index
-  # GET /index.xml
   def index
     @salesOrders = SalesOrder.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @salesOrders }
-    end
+    status       = SalesOrderStatus.all_with_status_id
+    render 'index', :locals => { :status => status }  
   end
   
-  # /sales_orders/1
-  # /sales_orders/1.xml
+  # /sales_orders/1 
   def show
     @salesOrder = SalesOrder.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @salesOrder }
-    end
   end
 end
---
+++ app/models/sales_order_status.rb
@@ -0,0 +1,17 @@
+#encoding: utf-8
+#author cway 2013-08-31
+
+class SalesOrderStatus < ActiveRecord::Base
+  #attr_accessible :entity_id, :status, :state, :customer_id, :grand_total, :created_at
+  self.table_name = "sales_order_status"
+
+  def all_with_status_id
+    status        = SalesOrderStatus.all
+    status_hash   = Hash.new
+
+    status.each do |state|
+      status_hash[state.status_id]  =  state.status_label
+    end
+    status_hash
+  end
+end
