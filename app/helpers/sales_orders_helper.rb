be654f05143b6f8e13fb2b28e974874015b1c3a0
cway <cway@knock-pro.local>
2013-09-02 14:53:39 +0800

    x

---
+++ app/helpers/sales_orders_helper.rb
@@ -0,0 +1,14 @@
+#encoding: utf-8
+#author cway 2013-07-18
+
+module SalesOrdersHelper 
+
+  def get_shipping_address( shipping_id ) 
+    shipping_address      =    SalesOrdersAddress.where({entity_id: shipping_id}).first
+    if shipping_address
+      "详细地址: "shipping_address.province << " " << shipping_address.city << " " << shipping_address.district << " " << shipping_address.street << ", 收件人: " . << shipping_address.addressee << ", 电话: " << shipping_address.telephone << ", 邮编: " + shipping_address.postcode
+    else
+      "收货地址不存在"
+    end
+  end
+end
---
+++ app/models/sales_orders_address.rb
@@ -0,0 +1,6 @@
+#encoding: utf-8
+#author cway 2013-09-02
+
+class SalesOrderAddress < ActiveRecord::Base
+  self.table_name = "sales_order_address"
+end
